331d0f9c6d132ed49b47f7f1ec5635754a62cda6
Anandkumar Patel <avpatel2005@gmail.com>
2023-06-28 11:01:44 -0400

    add components Navbar, ProductList, ProductDetails, Cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
 
 function App() {
-  return <div>Testing</div>;
+  return (
+    <div>
+      <Navbar />
+      <ProductList />
+      <ProductDetail />
+      <Cart />
+    </div>
+  );
 }
 
 export default App;
