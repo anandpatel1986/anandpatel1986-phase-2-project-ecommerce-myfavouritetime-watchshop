32a5bf124ffea19fae8913d7878eba45d5bcb3b3
Anandkumar Patel <avpatel2005@gmail.com>
2023-07-19 09:16:04 -0400

    add Routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import NavBar from "./components/NavBar/NavBar";
 import Products from "./components/Products/Products";
 import Product from "./components/Product/Product";
 import Cart from "./components/Cart/Cart";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Products />
-      <Product />
-      <Cart />
+      <Switch>
+        <Route exact path="/">
+          <Products />
+        </Route>
+        <Route path="/product/:productId">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="*">
+          <h1>Not Found</h1>
+        </Route>
+      </Switch>
     </>
   );
 }
