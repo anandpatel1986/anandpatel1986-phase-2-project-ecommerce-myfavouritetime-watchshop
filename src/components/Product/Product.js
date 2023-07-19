a605372a65fd605575d2d4770d98db76ef45f874
Anandkumar Patel <avpatel2005@gmail.com>
2023-07-19 12:51:19 -0400

    create state variable in Product and get productId using useParams

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,11 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 function Product() {
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState({});
+  const productId = useParams();
+
   return <div>Product</div>;
 }
 
