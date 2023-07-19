fbdbe5cd24b0345adf1356bcaa4efd7f222f9845
Anandkumar Patel <avpatel2005@gmail.com>
2023-07-19 12:57:39 -0400

    call useEffect and fetch details for product by ID

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,8 +3,18 @@ import { Link, useParams } from "react-router-dom";
 
 function Product() {
   const [loading, setLoading] = useState(true);
-  const [product, setProduct] = useState({});
-  const productId = useParams();
+  const [product, setProduct] = useState();
+  const { productId } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3333/products/${productId}`)
+      .then((r) => r.json())
+      .then((product) => {
+        setLoading(true);
+        setProduct(product);
+        setLoading(false);
+      });
+  }, [productId]);
 
   return <div>Product</div>;
 }
