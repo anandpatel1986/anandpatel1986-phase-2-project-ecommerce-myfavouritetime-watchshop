c3fe2f05fae40bb57d93ca79c66dee0cb36769ff
Anandkumar Patel <avpatel2005@gmail.com>
2023-07-19 13:10:46 -0400

    complete Product detail component, fetch product details by ProductID

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -16,7 +16,25 @@ function Product() {
       });
   }, [productId]);
 
-  return <div>Product</div>;
+  return (
+    <div className="container">
+      {loading ? (
+        <div className={"loader"}></div>
+      ) : (
+        <div className="productDetails">
+          <img src="product.image" alt="" />
+          <div className="info">
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <span className="price">{product.price}</span>
+            <button className="addToCart-btn" onClick={() => {}}>
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Product;
